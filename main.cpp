0d2693fcf1d617ab2d1d385c200146ca180d5f7e
Adriel <adrielhanzel@Adriels-M1-MBP.local>
2024-10-05 10:43:23 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -6,6 +6,54 @@
 
 using namespace std;
 
+// Node structure
+struct ReviewNode {
+    float rating;
+    string comment;
+};
+
+// Function prototypes
+void addToHead();
+void addToTail();
+void displayReviews();
+
 int main(){
-    cout << "Hello world\n";
+    int choice;
+    char anotherReview;
+
+    cout << "Which linked list method should we use?\n";
+    cout << "    [1] New nodes are added at the head of the linked list\n";
+    cout << "    [2] New nodes are added at the tail of the linked list\n";
+    cout << "Choice: ";
+    cin >> choice;
+
+    do{
+        float rating;
+        string comment;
+
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+
+        cout << "Enter review comments: ";
+        getline(cin, comment);
+
+        if (choice == 1){
+            addToHead();
+        } else{
+            addToTail();
+        }
+
+        cout << "Enter another review? Y/N: "
+    } while (anotherReview == 'Y' || anotherReview == 'y')
+}
+
+// Function definitions
+void addToHead(){
+
+}
+void addToTail(){
+
+}
+void displayReviews(){
+
 }
