3f94285251263a556af9311af6ac6b3db0a4e0ac
Adriel <adrielhanzel@Adriels-M1-MBP.local>
2024-10-05 10:33:14 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// COMSC-210 | Lab 18 | Adriel Chandra
+// IDE used: Visual Studio Code
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << "Hello world\n";
+}
